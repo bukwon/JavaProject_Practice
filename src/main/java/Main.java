8a5bcf811b25afca2757a70bd033cd1c45126dee
bukwon <dnu05043@naver.com>
2023-11-10 15:20:57 +0900

    1단계

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.println("== 명언 앱 ==");
+            System.out.print("명령) ");
+            String menu = sc.nextLine();
+            if(menu.equals("종료")) return;
+        }
     }
 }
