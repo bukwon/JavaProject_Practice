41d0e10261f800ae8fc627420f4c3390424b9c53
bukwon <dnu05043@naver.com>
2023-11-14 22:08:11 +0900

    6,7 단계 수정 작업 및 8단계 완료

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -39,6 +39,11 @@ public class Controller {
                 case 3:
                     removeList();
                     break;
+                case 4:
+                    pasteList();
+                    break;
+                default:
+                    System.out.println("해당 메뉴얼이 존재하지 않습니다.");
             }
         }
     }
@@ -49,6 +54,7 @@ public class Controller {
         else if (menu.equals("등록")) menuNum = 1;
         else if (menu.equals("목록")) menuNum = 2;
         else if (menu.equals("삭제")) menuNum = 3;
+        else if (menu.equals("수정")) menuNum = 4;
 
         return menuNum;
     }
@@ -84,27 +90,56 @@ public class Controller {
         List<WiseSaying> jsonList = json.loadFromJsonFile();
 
         // 삭제할 id와 일치하는 명언을 찾아서 제거
-       boolean remove = jsonList.removeIf(saying -> saying.getId() == pasteId);
+        boolean remove = jsonList.removeIf(saying -> saying.getId() == pasteId);
+        for (WiseSaying wise : list) {
+            if (wise.id == pasteId) {
+                list.remove(wise);
+                break;
+            }
+        }
 
-       if (remove) {
-           for (int i = pasteId; i < jsonList.size(); i++) {
-               jsonList.get(i).id--;
-           }
-           // 파일에 저장
-           json.saveToJsonFile(jsonList);
-           System.out.println(pasteId + "번 명언이 삭제되었습니다.");
-       }
-       else System.out.println(pasteId + "번 명언은 존재하지 않습니다.");
+        if (remove) {
+            for (int i = pasteId; i <= jsonList.size(); i++) { // 여기 부분 이하로 만들어야 함
+                jsonList.get(i - 1).id--;
+                list.get(i - 1).id--;
+            }
+            id--;    // 번호 하나 씩 앞당겨졌으니 -1
+            // 파일에 저장
+            json.saveToJsonFile(jsonList);
+            file.saveToTextFile(jsonList);
+            System.out.println(pasteId + "번 명언이 삭제되었습니다.");
+        } else System.out.println(pasteId + "번 명언은 존재하지 않습니다.");
     }
 
-        int divideWord (String menu){
-            int divideWord = 0;
-            if (menu.contains("?")) {                           // 수정 및 삭제 입력 시 입장
-                String[] s1 = menu.split("\\?");        // 1차 분할 (메뉴 기능 & id)
-                String[] s2 = s1[1].split("=");         // 2차 분할 (id & id 번호)
-                this.pasteId = Integer.parseInt(s2[1]);      // String형 숫자를 정수로 변환 (삭제 및 수정 시 이용)
-                divideWord = menuList(s1[0]);
-            } else divideWord = menuList(menu);            // 수정 및 삭제 외 입장
-            return divideWord;
-        }           // 메뉴 문자 나누기
+    void pasteList() {
+        boolean found = false;
+
+        for (WiseSaying wise : list) {
+            if (wise.id == pasteId) {
+                found = true;
+                System.out.println("명언(기존) : " + wise.content);
+                System.out.print("명언 : ");
+                wise.content = sc.nextLine();
+                System.out.println("작가(기존) : " + wise.author);
+                System.out.print("작가 : ");
+                wise.author = sc.nextLine();
+                json.saveToJsonFile(list);
+            }
+        }
+        if (found) {
+            System.out.println(pasteId + "번 수정 완료");
+        }
+        else System.out.println("해당 번호가 없습니다.");
     }
+
+    int divideWord(String menu) {
+        int divideWord = 0;
+        if (menu.contains("?")) {                           // 수정 및 삭제 입력 시 입장
+            String[] s1 = menu.split("\\?");        // 1차 분할 (메뉴 기능 & id)
+            String[] s2 = s1[1].split("=");         // 2차 분할 (id & id 번호)
+            this.pasteId = Integer.parseInt(s2[1]);      // String형 숫자를 정수로 변환 (삭제 및 수정 시 이용)
+            divideWord = menuList(s1[0]);
+        } else divideWord = menuList(menu);            // 수정 및 삭제 외 입장
+        return divideWord;
+    }           // 메뉴 문자 나누기
+}
